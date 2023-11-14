89168100287b92f5492c534c6df5c04d98010cab
sallinen <alex.sallinen@gmail.com>
2023-11-14 15:52:04 +0100

    Correct optimiser

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 # git clone https://github.com/lighthea/MiniMeditron-Prototype.git
 export PATH=$PATH:/home/sallinen/.local/bin
+pip3 install --pre torch torchvision torchaudio -f https://download.pytorch.org/whl/nightly/cu113/torch_nightly.html
 pip install -r requirements.txt
 mkdir data/TF-IDF
 sudo apt update && sudo apt install -y unzip
