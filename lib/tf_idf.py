0a3b6ee95f967eb166f245466cf6818b13e3c211
sallinen <alex.sallinen@gmail.com>
2023-11-17 19:54:10 +0100

    Type problems

--- lib/tf_idf.py
+++ lib/tf_idf.py
@@ -8,38 +8,31 @@ from tqdm import tqdm
 
 from lib.utils import yield_structured_obj
 
-def preprocess_json_for_bm25(json_string: str):
-    try:
-        # Load the JSON string into a Python dictionary
-        json_string = json_string.replace("\'", "\"")
-        data = json.loads(json_string)
 
-        # Initialize an empty list to store the cleaned words
-        cleaned_words = []
+def preprocess_json_for_bm25(json_obj: dict):
+    # Load the JSON string into a Python dictionary
 
-        # Iterate over key-value pairs
-        for key, value in data.items():
-            # Consider both keys and values, ensure they are strings
-            key_value_str = str(key) + " " + str(value)
+    data = json_obj
+    # Initialize an empty list to store the cleaned words
+    cleaned_words = []
 
-            # Convert to lowercase
-            key_value_str = key_value_str.lower()
+    # Iterate over key-value pairs
+    for key, value in data.items():
+        # Consider both keys and values, ensure they are strings
+        key_value_str = str(key) + " " + str(value)
 
-            # Split into words and extend the list
-            cleaned_words.extend(key_value_str.split())
+        # Convert to lowercase
+        key_value_str = key_value_str.lower()
+
+        # Split into words and extend the list
+        cleaned_words.extend(key_value_str.split())
 
         return cleaned_words
 
-    except json.JSONDecodeError:
-        print(json_string)
-        # Handle the case where the input is not a valid JSON string
-        raise ValueError("Invalid JSON string provided.")
-
-
 def process_folder_for_bm25(folder_path: str) -> list[str]:
     # Loop through each file in the folder
-    for json_string in yield_structured_obj(folder_path):
-        yield preprocess_json_for_bm25(json_string)
+    for json_obj in yield_structured_obj(folder_path):
+        yield preprocess_json_for_bm25(json_obj)
 
 
 def init_bm25(corpus_folder_path: str) -> BM25Okapi:
@@ -57,6 +50,6 @@ def retrieve_n_best_guidelines(query: str, bm25: BM25Okapi, guidelines: list[str
 def batch_bm25(dataset: Dataset, guideline_folder: str, n: int = 3):
     print("Initializing BM25 model")
     bm25 = init_bm25(guideline_folder)
-    guidelines = yield_structured_obj(guideline_folder)
+    guidelines = list(map(lambda x: str(x), yield_structured_obj(guideline_folder)))
     dataset.map(lambda x: {"context": retrieve_n_best_guidelines(x["query_column"], bm25, guidelines, n=n)})
     return dataset
