8413f587cd995ecdcaa584f8b2940fed1f901a6f
sallinen <alex.sallinen@gmail.com>
2023-11-19 15:59:10 +0100

    Better image building

--- scripts/update_docker_image.sh
+++ scripts/update_docker_image.sh
@@ -2,11 +2,24 @@ set -e  # exit on error
 
 VERSION=1
 REGISTRY=ic-registry.epfl.ch
+IMG_PROJECT=mlo-sallinen-meditron
+echo "You have to set the following environment variables:"
+echo "  - IMG_NAME: name of the image (MANDATORY)"
+echo "  - REGISTRY: registry to push the image to (defaults to ic-registry.epfl.ch)"
+echo "  - IMG_PROJECT: project name (defaults to mlo-sallinen-meditron)"
+echo "  - VERSION: version of the image (defaults to 1)"
 
-docker build . -t $IMG_NAME:$VERSION
-docker tag $IMG_NAME:$VERSION $REGISTRY/$IMG_NAME:$VERSION
-docker tag $IMG_NAME:$VERSION $REGISTRY/$IMG_NAME:latest
-docker push $REGISTRY/$IMG_NAME:$VERSION
-docker push $REGISTRY/$IMG_NAME:latest
-docker rmi $REGISTRY/$IMG_NAME:$VERSION
-docker rmi $REGISTRY/$IMG_NAME:latest
+if [ -z "$IMG_NAME" ]; then
+    echo "IMG_NAME is not set"
+    exit 1
+fi
+
+IMG_NAME=$IMG_PROJECT/$IMG_NAME
+
+docker build . -t "$IMG_NAME":$VERSION
+docker tag "$IMG_NAME":$VERSION $REGISTRY/"$IMG_NAME":$VERSION
+docker tag "$IMG_NAME":$VERSION $REGISTRY/"$IMG_NAME":latest
+docker push $REGISTRY/"$IMG_NAME":$VERSION
+docker push $REGISTRY/"$IMG_NAME":latest
+docker rmi $REGISTRY/"$IMG_NAME":$VERSION
+docker rmi $REGISTRY/"$IMG_NAME":latest
