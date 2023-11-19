b0e218da3cbafcffb8040d071d12cb347ef936b0
sallinen <alex.sallinen@gmail.com>
2023-11-19 15:53:54 +0100

    Modular Training + image

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -1,8 +1,12 @@
+REGISTRY=ic-registry.epfl.ch
+IMG_PROJECT=mlo-sallinen-meditron
+IMG_NAME=prot0
+
 runai submit \
 		--name "sandbox" \
 		--interactive \
 		--gpu 1 \
-		--image ic-registry.epfl.ch/mlo/pytorch:latest \
+		--image $REGISTRY/$IMG_PROJECT/$IMG_NAME:latest \
 		--pvc runai-mlo-sallinen-mlodata1:/mlodata1 \
 		--pvc runai-mlo-sallinen-mloraw1:/mloraw1 \
 		--pvc runai-mlo-sallinen-scratch:/scratch \
