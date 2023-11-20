9c037ebb4d3d74c23e8f96b9d0a2f1d68899f815
sallinen <alex.sallinen@gmail.com>
2023-11-20 17:59:17 +0100

    Small error token

--- src/reinforcement_training_block.py
+++ src/reinforcement_training_block.py
@@ -36,7 +36,6 @@ def main():
         return torch.rand(10)
 
     train_dataset = load_dataset(config, tokenizer, None, with_context=True, with_token=False, with_output=False)
-    train_dataset = train_dataset.rename_column("text", "query")
     train_dataset = train_dataset.rename_column("labels", "response")
 
     def tokenize(sample):
