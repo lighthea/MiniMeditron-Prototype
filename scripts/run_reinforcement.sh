a12a668582a450197266b75e39b12b4383afece7
m.zhang2490@gmail.com <m.zhang2490@gmail.com>
2023-11-27 14:52:57 +0100

    Fix config once again

--- scripts/run_reinforcement.sh
+++ scripts/run_reinforcement.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export CONF_FILE=../conf/config_ppo_contrastive_m2
+export CONF_FILE=../conf/config_ppo_contrastive_m2.json
 
 python ../src/reinforcement_training_block.py $CONF_FILE
