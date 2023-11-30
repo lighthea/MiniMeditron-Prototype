408aed3fc1c6fb18202da09074c2ef14eb1f819c
m.zhang2490@gmail.com <m.zhang2490@gmail.com>
2023-11-30 18:03:02 +0100

    idk

--- scripts/local_commit.sh
+++ scripts/local_commit.sh
@@ -1,4 +1,4 @@
-scp -P 2244 root@127.0.0.1:MiniMeditron-Prototype/scripts/diff.patch diff.patch
+scp -P 2255 root@127.0.0.1:MiniMeditron-Prototype/scripts/diff.patch diff.patch
 git apply diff.patch
 git add -A
 git commit -m "$1"
