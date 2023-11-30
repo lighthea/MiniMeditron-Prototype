3fb90e457f28d750126b472b7944c9ca08abed40
m.zhang2490@gmail.com <m.zhang2490@gmail.com>
2023-11-30 13:06:15 +0100

    Update cluster ssh

--- scripts/cluster_ssh.sh
+++ scripts/cluster_ssh.sh
@@ -1,4 +1,5 @@
 
+pip install pycryptodome
 export AUTHORIZED_KEYS="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFhFZAhJNOG594WRcwV/zkwRQ2WWYAqpTGaq1dNGg0sD michael@DESKTOP-KP33C2M"
 echo "Install ssh server"
 sudo apt-get update
