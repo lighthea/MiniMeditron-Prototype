027eb068517b7910b3b8f2b82c8b07b13eff8576
Guillaume Boyé <guillaume.boye@epfl.ch>
2023-12-06 20:45:09 +0100

    Push some things that make some stuff

--- src/gen_wikidata_db.py
+++ src/gen_wikidata_db.py
@@ -1,22 +1,28 @@
 import os
 import sys
+import time
 from os.path import join, exists
 from os import listdir
 
-import tqdm
 import requests
 import json
+from tqdm import tqdm
 
 from qwikidata.entity import WikidataItem, WikidataClaimGroup
 from qwikidata.json_dump import WikidataJsonDump
 from qwikidata.utils import dump_entities_to_json
 
-WIKIDATA_COMPRESSED_FILE_PATH = join("..", "wikidata", "wikidata-20220103-all.json")
+WIKIDATA_COMPRESSED_FILE_PATH = join("..", "wikidata", "wikidata-20220103-all.json.gz")
+WIKIDATA_OUT_FILE_NAME = join(".", "diseases-out.json")
+
 
 P_INSTANCE_OF = "P31"
 P_SUBCLASS_OF = "P279"
-Q_CLASS_OF_DISEASE = "Q112193867"
-Q_DISEASE = "Q12136"
+Q_MATCHED = set([
+    "Q112193867",
+    "Q2057971",
+    "Q12136"
+])
 
 # create an instance of WikidataJsonDump
 wjd = WikidataJsonDump(WIKIDATA_COMPRESSED_FILE_PATH)
@@ -30,11 +36,34 @@ def find_ids(claim_group: WikidataClaimGroup) -> list[str]:
 
 # create an iterable of WikidataItem 
 diseases = []
-for ii, entity_dict in enumerate(wjd):
-    if entity_dict["type"] == "item":
-        entity = WikidataItem(entity_dict)
-        instance_of = set(find_ids(entity.get_claim_group(P_INSTANCE_OF)))
-        subclass_of = set(find_ids(entity.get_claim_group(P_SUBCLASS_OF)))
+t1 = time.time()
+try:
+    for ii, entity_dict in tqdm(enumerate(wjd), total=107795041):
+        if entity_dict["type"] == "item":
+            entity = WikidataItem(entity_dict)
+            instance_of = find_ids(entity.get_claim_group(P_INSTANCE_OF))
+            subclass_of = find_ids(entity.get_claim_group(P_SUBCLASS_OF))
 
-        if Q_DISEASE in subclass_of or Q_CLASS_OF_DISEASE in instance_of or Q_DISEASE in instance_of or Q_CLASS_OF_DISEASE in subclass_of:
-            diseases.append(entity)
+            # if any(x in Q_MATCHED for x in instance_of + subclass_of):
+            #     diseases.append(entity)
+
+            if "Q112193867" in instance_of or "Q2057971" in instance_of or "Q12136" in instance_of or \
+                "Q112193867" in subclass_of or "Q2057971" in subclass_of or "Q12136" in subclass_of:
+                diseases.append(entity)
+
+
+        if ii % 100000 == 0 and ii > 0:
+            t2 = time.time()
+            dt = t2 - t1
+            tqdm.write("Found {} records among {} entities [entities/s: {:.2f}]".format(
+                len(diseases), ii, ii / dt
+            ))
+except KeyboardInterrupt as e:
+    print("Interrupting...")
+
+# write the iterable of WikidataItem to disk as JSON
+t2 = time.time()
+dt = t2 - t1
+print("Found {} records in {:.1f}min".format(len(diseases), dt/60.0))
+dump_entities_to_json(diseases, WIKIDATA_OUT_FILE_NAME)
+wjd_filtered = WikidataJsonDump(WIKIDATA_OUT_FILE_NAME)
