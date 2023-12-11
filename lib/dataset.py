0f748695fd3f3e102e87e72e0bc6ba182c2b6a42
m.zhang2490@gmail.com <m.zhang2490@gmail.com>
2023-12-11 12:00:40 +0100

    dpo_cos_upsample is wrong but anyway

--- lib/dataset.py
+++ lib/dataset.py
@@ -235,7 +235,6 @@ def load_dataset(config: dict, tokenizer) -> DatasetDict:
 
     # Construct the raw dataset
     dataset = construct_raw_dataset(config)
-    dataset = dataset.select(range(3))
 
     # Insert potential embedding
     dataset = fill_with_embeddings(config, dataset)
